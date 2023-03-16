628ad58a9bbfe513f679dea4c660e8d5690d3d10
lstgh <lstgh@pc>
2023-03-16 14:37:08 +0600

    added more comments and readme guide

--- MainBuildingScript.ps1
+++ MainBuildingScript.ps1
@@ -1,6 +1,5 @@
-# If any error occurs, execution will be stopped
+# If any error occurs, execution of the script will be stopped
 $ErrorActionPreference = "Stop";
-
 # Payload for notifications. Variables can be freely adjusted
 $request_body = @"
 {
@@ -46,17 +45,15 @@ $request_body = @"
   ]
 }
 "@
-
 # Main executable files and libraries location
 $env:Path += ";$env:WORKSPACE\Root\Tools\Miktex\texmfs\install\miktex\bin\x64"
 $env:Path += ";$env:WORKSPACE\Root\Tools\Pandoc"
-
 # Here goes your remote shared drive authentication
 $secpasswd = ConvertTo-SecureString "$env:ACCOUNT" -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("ACCOUNT", $secpasswd)
 Write-Host "Creating temporary mapped network drive"
 New-PSDrive -Name "Storage" -PSProvider "FileSystem" -Root "PATH" -Credential $mycreds
-
+# Cleaning temp folders from the previous launches
 Write-Host "Checking for temporary files"
 $PathTemp = Test-Path -Path "$env:WORKSPACE\tex2pdf*"
 if ($PathTemp) {
@@ -66,7 +63,7 @@ if ($PathTemp) {
 elseif (!$PathTemp) {
   Write-Host "No temporary files detected"
 }
-
+# To avoid redownloading tools each time, here is a check for their presence
 Write-Host "Checking for Pandoc and MikTeX"
 $PathTools = Test-Path -Path "$env:WORKSPACE\Root\Tools"
 if (!$PathTools) {
@@ -77,7 +74,6 @@ if (!$PathTools) {
 elseif ($PathTools) {
   Write-Host "Tools were found, proceeding"
 }
-
 $tempFolder = "TempFolder"
 $isExist = Test-Path -Path $env:WORKSPACE\$tempFolder
 if (!$isExist) {
@@ -88,19 +84,22 @@ else {
   Remove-Item -Recurse -Path "$env:WORKSPACE\$tempFolder"
   mkdir $env:WORKSPACE\$tempFolder
 }
-
 # Handler for reusable text parts
+# Parent document with a pattern ~Some_reused_text~
+# Will be replaced onto content of the Some_reused_text.md file in the repository root
 try {
+  # Variable to work only with specific documents set
   $ext = $env:Extension
   $mainFiles = Get-ChildItem -Recurse -Path "$env:WORKSPACE\*$ext*\*$ext*.md"
+  # Symbol to detect reusable text inside of the main documents
   $serviceSymbol = "~"
-
+  # A cycle to iterate through each document and collect all reusable parts
   foreach ($mainFile in $mainFiles) {
     $tempKeyWordList = Get-Content $mainFile | Select-String -Pattern "$serviceSymbol*$serviceSymbol"
     if ($tempKeyWordList.Count -gt 0) {
       $keyWords = $tempKeyWordList -Replace $serviceSymbol
       $content = Get-Content $mainFile
-
+      # Replacement procedure
       foreach ($replacement in $keyWords) {
         $fullContent = Get-Content "$env:WORKSPACE\*$replacement*" -Raw
         $content = $content -Replace $replacement, $fullContent
@@ -111,34 +110,37 @@ try {
       Set-Content -Path $env:WORKSPACE\$tempFolder\Full_$fileName -Value $content
       Set-Location $env:WORKSPACE\$tempFolder
     }
+    # Fallback for documents, where no reusable parts are present
     else {
       Write-Host "Found MD without replacement parts"
       Set-Location $env:WORKSPACE\$ext
       Copy-Item *.md -Destination $env:WORKSPACE\$tempFolder
     }
   }
-  
+  # Detecting temporary files, used in preprocessing
   Set-Location $env:WORKSPACE\$tempFolder
   $prePreFilesToBeRemoved = Get-ChildItem -Recurse -Include "Full_*"
-
   if ($prePreFilesToBeRemoved.Count -gt 0) {
     $preFilesToBeRemoved = New-Object -TypeName 'System.Collections.ArrayList'
     foreach ($file in $prePreFilesToBeRemoved) {
       $preFilesToBeRemoved.Add([System.IO.Path]::GetFileName($file))
     }
+    # Returning the original name
     $filesToBeRemoved = $preFilesToBeRemoved -Replace "Full_*"
+    # Deleting previously collected files
     Remove-Item * -Include $filesToBeRemoved
   }
-
+  # Handler for images from relative and friendly to absolute for Pandoc
+  # ![**Image description**](./Folder/Image.png) - source
+  # ![**Image description**](D:\Folder\Folder\Image.png) - after handling
   $filesList = Get-ChildItem -Recurse -Path "$env:WORKSPACE\$tempFolder\*.md"
-
-  # Handler for reusable images
   foreach ($file in $filesList) {
+    # Collecting images by wildcard
     $preImagePath = Get-Content "$file" | Select-String -Pattern "\./.*\.png"
+    # Preprocessing found images
     $imagePath = $preImagePath -Replace ".*\(" -Replace "\)" -Replace ".*\{" -Replace "\}.*"
-
     $newContent = Get-Content $file
-    
+    # Processing of shared and unique images
     foreach ($image in $imagePath) {
       if ($image -Match ".*SharedImages.*") {
         $newImage = $image -Replace "\./", "$env:WORKSPACE/"
@@ -146,19 +148,17 @@ try {
       elseif ($image -NotMatch ".*SharedImages.*") {
         $newImage = $image -Replace "\./", "$env:WORKSPACE/$ext/"
       }
-
       $newImage = $newImage -Replace "\\", "/"
-        
       $newContent = $newContent -Replace $image, $newImage
     }
-    
     $fileName = [System.IO.Path]::GetFileName($file)
     Set-Content -Path ".\$fileName" -Value $newContent
   }
-  # A command to Pandoc to convert all MD files in the directory to PDF ones
+  # A command to Pandoc to convert all MD files in the directory to PDF ones after all preprocessing stages for final documents versions
   Get-ChildItem -r -i *.md | ForEach-Object { $pdf = $_.directoryname + "\" + $_.basename + ".pdf"; pandoc $_.name --pdf-engine=xelatex -o $pdf }
 }
 catch {
+  # Notification about building process failure
   Write-Host $_
   $build_status = "Build failed"
   $status_color = "#ff0000"
@@ -167,21 +167,21 @@ catch {
   Invoke-RestMethod -Uri "WEB_HOOK_ADDRESS" -Method POST -Body $request_body
   exit 1
 }
-
+# Example of custom path for uploading output PDF files, based on the variables from Jenkins job
 Write-Host "Documentation is ready"
 try {
   if ($env:ANY_VARIABLE -eq "ANY REQUIRED VALUE") {
-    Write-Host("Some action description with different destination" + $env:BUILD_NUMBER)
+    Write-Host("Some action description with different destination1")
     New-Item -ItemType Directory -Force -Path "PATH\$env:BUILD_NUMBER"
     Copy-Item *.pdf -Destination "PATH\$env:BUILD_NUMBER"
   }
   else {
-    Write-Host("Some action description with different destination " + $env:Extension)
+    Write-Host("Some action description with different destination2")
     Copy-Item *.pdf -Destination "PATH\$env:Extension"
   }
 }
-
 catch {
+  # Notification about failure of PDFs upload to shared drive
   Write-Host $_
   $build_status = "Can\'t upload files"
   $status_color = "#f2ff00"
@@ -190,7 +190,7 @@ catch {
   Invoke-RestMethod -Uri "WEB_HOOK_ADDRESS" -Method POST -Body $request_body
   exit 1
 }
-
+# Successful build notification
 $build_status = "Build success"
 $status_color = "#1fc90c"
 $request_body = $request_body -Replace "build_status", $build_status
